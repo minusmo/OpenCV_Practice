0164a16d2281f1bd71caecd50337f6cce040c5f9
hojooneum <bldolphin96@gmail.com>
2023-03-09 01:11:26 +0900

    feat: hexagram completed

--- hexagram/main.cpp
+++ hexagram/main.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "OpenGL/gl.h"
 
+#define ORIGIN 0.0f
+#define POINTS 3
+#define EDGE_SIZE 1.0f
+const float vertices[POINTS][2] = {
+        {-EDGE_SIZE/2, -EDGE_SIZE/2},
+        {EDGE_SIZE/2, -EDGE_SIZE/2},
+        {ORIGIN, EDGE_SIZE/2}
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hexagram", NULL, NULL);
+
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) { glfwTerminate(); return -1; }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        for (auto point : vertices) {
+            glVertex2f(point[0], point[1]);
+        }
+        for (auto point : vertices) {
+            glVertex2f(point[0], -point[1] - EDGE_SIZE/4);
+        }
+        glEnd();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
